51e77c29f820691765a0adb4f5d4739e7bbc6ffc
OmkarUdande <omkarudande86@gmail.com>
2024-11-08 02:12:32 +0000

    Conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "omkar"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// "33"==> 33
+// "33 abc"==> NaN
+// true => 1; false=> 0
+
+let isLoggedIn = "omii"
+let booleanIsLoggendIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggendIn);
+
+//nots=>:
+// 1=> true: 0 =>false
+// ""=> false
+// "omkar" =>true
+
+
+let someNumber = 33
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
