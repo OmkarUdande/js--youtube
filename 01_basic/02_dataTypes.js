9a0a736e8cd9748723dc78a4b2145a13a388c9f3
OmkarUdande <omkarudande86@gmail.com>
2024-11-08 01:48:53 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code in new version 
+
+// alert(3+3) // we are using nodejs not browser 
+
+console.log(3 + 3);
+
+
+let name = "omkar"
+let age = 24
+let isLoggedIn =true
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standloane value 
+// undefind => value is not defind
+// symbol =>unique 
+
+
+//Object
+
+console.log(typeof undefined); //(type =undeifnd)
+console.log(typeof null);  // (type = object)
+
