ae961a2d11c49f7892e127779c5e9b92c3a96a61
OmkarUdande <omkarudande86@gmail.com>
2024-11-09 03:12:08 +0000

    conversionOpration

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -24,5 +24,63 @@ let booleanIsLoggendIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber =String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ***************Oprations**************
+
+let value = 3
+let negValue = -value
+//console.log(negValue); // -3
+
+
+//basic Oprations:==
+// console.log(2+2);
+// console.log(2-3);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " omkar"
+let str3 = str1+str2
+// console.log(str3); // hello omkar
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "3");
+// console.log("1" + 2 + 3);
+
+// console.log( (3 + 4) * (5 +"4"));
+
+// console.log(+true);  // 1
+// console.log(+"");    // 0
+
+let num1 ,num2, num3
+
+num1 = num2 = num3 =2 +2
+
+let gameCounter = 100
+gameCounter++
+// ++gameCounter
+// console.log(gameCounter);          // 101
+
+//***prefix***
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);        //"x:4, y:3"
+
+//***postfix***
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a},b:${b}`);         // "a:4, b:4"
+
+
+// link to  help for study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
