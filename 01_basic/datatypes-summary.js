22e584474dfd6727673fdf3914772dee8d94cb7f
OmkarUdande <omkarudande86@gmail.com>
2024-11-09 09:23:37 +0000

    datatypes-summary

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,50 @@
+// ***primitive***
+
+// 7 Types : String, Number, Boolean, null, Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoogedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id == anotherId );
+
+const bigNumber = 274378293948584832n
+
+
+
+
+
+//Reference(Non primitive)
+
+//Array, Objects,Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+
+let myobject={
+    name:"omkar",
+    age:23,
+}
+
+
+const myfuntions = function(){
+    console.log("Hello World");
+    
+}
+
+// type check (typeof)
+// console.log(typeof scoreValue);
+// console.log(typeof score);
+// console.log(typeof anotherId);
+// console.log(typeof Objects);
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
