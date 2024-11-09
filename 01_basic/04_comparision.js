09746288b27aaa80d7086cc25c3ae5ec37d65f86
OmkarUdande <omkarudande86@gmail.com>
2024-11-09 04:16:00 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,27 @@
+//***Comparision***
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2"> 1);   //true
+// console.log("02"> 1);  //true
+
+console.log(null > 0);    //F
+console.log(null == 0);   //F
+console.log(null >= 0);   //T
+
+console.log(undefined == 0); //F
+console.log(undefined > 0);  //F
+console.log(undefined < 0);  //F
+
+
+
+// ===
+console.log("2"=== 2);
+
+
+
