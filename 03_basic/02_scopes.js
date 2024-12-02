64100ee6fa0f455e61e60d6aab6361a005a0e423
OmkarUdande <omkarudande86@gmail.com>
2024-12-02 10:18:36 +0000

    function_scopes

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -1,8 +1,6 @@
 //  scope:= {}
 // not use in Var
 
-// var c = 300
-
 let a = 300   // out side the if condtion the scope is the  callad as the  "globle scope" 
 
 if (true) {
@@ -11,6 +9,51 @@ if (true) {
     console.log("INNER: ", b);     // inside the if condiion this is scope call as the  "Block scope" 
     
 }
-console.log(a);
+
+// console.log(a);
 // console.log(b);
-// console.log(c);
+
+// ******************nested Scope:=******************
+function one(){
+    const username = "OMKAR"
+
+    function two(){
+        const webside = "youTube"
+        console.log(username);
+    }
+    // console.log(webside);
+
+    two()
+        
+}
+one()
+
+if (true) {
+    const username = "Omkar"
+    if (username == "Omkar"){
+        const webside = " Youtube"
+        console.log(username + webside);
+        
+    }
+    
+}
+
+
+
+// *****************  Intersting************************
+
+
+function addone(num){
+    return num + 1
+}
+
+console.log(addone(5));
+
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5));
+
+
+
