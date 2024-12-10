0a63a767dcfcf435eabbe7deca1fdd63e7288c5c
OmkarUdande <omkarudande86@gmail.com>
2024-12-10 04:18:23 +0000

    if and else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,60 @@
+const isUserloggedIn= true
+
+// **********
+// const Temprature = 41
+// if (Temprature< 50) {
+//     console.log("yes courrnt  temprture is less then 50");     
+// } else{
+//     console.log("temp iS GRATER  then 50");
+
+// }
+// console.log("  execute ");
+
+// <, >, <=, >=, ==, != , ===, !==
+
+
+// **************
+
+// const score = 200
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User Power : ${power}`);    
+// }
+// console.log(`User Power : ${power}`); // not run this line 
+
+
+// *********:=
+
+// const balance = 1000
+// if(balance>500) console.log("Test");
+
+// if (balance < 500) {
+//     console.log("balance is less then 500 ");  
+
+// }  else if (balance < 750) {
+//     console.log("balance is less then 750 ");   
+
+// }  else if (balance < 900) {
+//     console.log("balance is grater 900 ");  
+    
+// } else{
+//     console.log("balance is less then 1200 ");  
+
+// }
+
+
+
+// ******: shopping 
+const UserloggedIn = true
+const DebitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (UserloggedIn && DebitCard && 2==3) {
+    console.log("Allow to bye course");   
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User Logged in");
+    
+}
